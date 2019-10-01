cf9b24cfa06d648d5851f325c908e08e52c46275
Alan Voss <alan.voss@postmates.com>
2019-10-01 18:38:12 -0500

    wip: all game engine there -- remains only partially tested

--- lib/ctf/game.ex
+++ lib/ctf/game.ex
@@ -19,51 +19,77 @@ defmodule Ctf.Game do
     }
   end
 
+  # returns status (:draw, {:win, player})
   def play(%__MODULE__{} = game) do
-    perform_game_loop(:ok, game, [])
+    perform_game_loop(:ok, game, [], 0)
   end
 
-  defp perform_game_loop(:ok, game, frames) do
+  # limit to 100000 loops
+  defp perform_game_loop(_, _, frames, 100000), do: {:draw, frames}
+  defp perform_game_loop(:ok, game, frames, count) do
     {move_lists, players} =
-      Enum.reduce(game.board.players, {[], []}, fn player, {move_lists, players} ->
+      Enum.reduce(game.board.players, {[], []}, fn player, {move_lists, acc} ->
         {move_list, new_state} = apply(player.module, :turn, [game, player.state])
-        {move_lists ++ [move_list], players ++ [%Player{player | state: new_state}]}
+        {move_lists ++ [move_list], acc ++ [%Player{player | state: new_state}]}
       end)
 
-    {status, game, new_frames} =
+    {status, new_frames} =
       lockstep(move_lists, 3)
-      |> advance_steps(:ok, %__MODULE__{game | board: %{game.board | players: players}}, [])
+      |> Enum.reduce({:ok, [game]}, fn {p1_move, p2_move}, {status, [latest_frame | _] = acc} ->
+           if status == :ok do
+             # displacement calcs to make sure we do moves in proper order
+             # if player 1 were to move to where player 2 is currently, then do
+             # moves in the opposite order, as player 2 might move out of the way
+             # by the time player 1 moves.  if player 2 moves into player 1, we
+             # are okay with that, because we know that player 1 is also going
+             # to move into player 2.
 
-    perform_game_loop(status, game, frames ++ new_frames)
+             p1_will_move_into_p2 =
+               if elem(p1_move, 0) == :move do
+                 [%Player{number: 1, x: x, y: y} = player1, player2] = players
+                 {displace_x, displace_y} = Player.get_displacement(player1)
+                 new_cell_x = displace_x + x 
+                 new_cell_y = displace_y + y
+                 player2 in(Board.get_cell_contents(latest_frame.board, new_cell_x, new_cell_y))
+               else
+                 false
+               end
+
+             move_players = Enum.zip([p1_move, p2_move], players)
+             move_players_maybe_reversed =
+               cond do
+                 elem(p1_move, 0) != :fire && elem(p2_move, 0) == :fire ->
+                   # have to perform fires first in same lockstep
+                   Enum.reverse(move_players)
+                 p1_will_move_into_p2 ->
+                   # have to perform moves in order to prevent timing problems (see above)
+                   Enum.reverse(move_players)
+                 true ->
+                   #no need to reverse
+                   move_players
+               end
+
+             # clear events on each call to create_frame
+             frame_with_cleared_events =
+               %__MODULE__{latest_frame | board: %Board{latest_frame.board | events: []}}
+
+             {frame_status, new_frames} = create_frame(move_players_maybe_reversed, :ok, [frame_with_cleared_events])
+             {frame_status, new_frames ++ acc}
+           else
+             # TODO: could probably use :halt here, but it's only 3, so I'll look later
+             {status, acc}
+           end
+         end)
+
+    [_initial_seed_frame | rest_frames] = Enum.reverse(new_frames)
+    perform_game_loop(status, game, frames ++ Enum.reverse(rest_frames), count+1)
   end
-  defp perform_game_loop(status, _, frames), do: {status, frames}
+  defp perform_game_loop(status, _, frames, _), do: {status, frames}
 
-  defp advance_steps([{p1_move, p2_move} | rest], :ok, game, frames) do
-    require IEx
-    IEx.pry
-
-
-    # after each set of 2, evaluate win, loss
-    # have to perform fires first in same lockstep
-    # have to perform moves in order to prevent timing problems
-    # -> ^, has to be performed in opposite order in same lockstep
-    # clear collisions on each call to create_frame
-    # not only need to detect collision on move, but also record if someone else landed in the space in the
-    #   same frame as the one being collided in
-
-
-    move_players = Enum.zip([p1_move, p2_move], game.board.players)
-    {frame_status, new_frames} = create_frame(:ok, move_players, [game])
-    [newest_game | _] = Enum.reverse(new_frames)
-    advance_steps(rest, frame_status, newest_game, frames ++ new_frames)
-  end
-  defp advance_steps([], :ok, game, frames), do: {:ok, game, frames}
-  defp advance_steps(_, status, game, frames), do: {status, game, frames}
-
-  defp create_frame([], status, frames) do
+  defp create_frame(status, [], frames) do
     # reverse and return all but final frame
     [_initial_game | rest_frames] = Enum.reverse(frames)
-    {status, rest_frames}
+    {status, Enum.reverse(rest_frames)}
   end
   defp create_frame(status, [{{:fire, count}, %Player{x: x, y: y} = player} | rest_steps], frames) do
     [newest_frame | rest_frames] = frames
@@ -73,7 +99,7 @@ defmodule Ctf.Game do
         new_hit_player = %Player{hit_player | health_points: health_points - 1}
         updated_frame =
           %__MODULE__{newest_frame |
-            board: %Board{newest_frame.board|
+            board: %Board{newest_frame.board |
               players: List.replace_at(
                 newest_frame.board.players,
                 number - 1,
